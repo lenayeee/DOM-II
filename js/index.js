382522aedf03c24fab9b544a82ca3bca28396af3
Leonard Ayala <leonardayala@Leonards-MBP.hsd1.ct.comcast.net>
2020-03-11 18:24:51 -0400

    still working on

--- js/index.js
+++ js/index.js
@@ -1,1 +1,180 @@
 // Your code goes here
+// function clickEventHandler(event) {
+//     console.log(event.type + '!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+//   }
+//   document.querySelector('a').addEventListener('click', clickEventHandler)
+//   document.querySelector('nav').addEventListener('click', clickEventHandler)
+//   document.querySelector('header').addEventListener('click', clickEventHandler)
+//   document.body.addEventListener('click', clickEventHandler)
+//   document.addEventListener('click', clickEventHandler)
+//   window.addEventListener('click', clickEventHandler)
+
+
+
+// 1. Nav bar highlight color effect
+const navColoring = document.querySelector(".nav-container .nav");
+navColoring.addEventListener("mouseover", function(event){
+    event.target.style.color = "purple"
+    setTimeout(function() {
+    event.target.style.color = "";
+
+    }, 500);
+})
+
+
+// let test = document.getElementById("test");
+  
+// // This handler will be executed only once when the cursor
+// // moves over the unordered list
+// test.addEventListener("mouseenter", function( event ) {   
+//   // highlight the mouseenter target
+//   event.target.style.color = "purple";
+
+//   // reset the color after a short delay
+//   setTimeout(function() {
+//     event.target.style.color = "";
+//   }, 500);
+// }, false);
+
+// 2. Map photo scroll zoom effect
+function zoom(event) {
+    event.preventDefault();
+    
+    scale += event.deltaY * -0.01;
+    
+    // Restrict scale
+    scale = Math.min(Math.max(.125, scale), 4);
+    
+    // Apply scale transform
+    advImg.style.transform = `scale(${scale})`;
+    }
+    
+    let scale = 1;
+    const advImg = document.querySelector('div .img-content');
+    // busImage.onwheel = zoom;
+    advImg.addEventListener('wheel', zoom);
+
+
+// 3 DRAG EVENT WITH DROPZONE
+var dragged;
+
+/* events fired on the draggable target */
+document.addEventListener("drag", function(event) {
+
+}, false);
+
+document.addEventListener("dragstart", function(event) {
+  // store a ref. on the dragged elem
+  dragged = event.target;
+  // make it half transparent
+  event.target.style.opacity = .5;
+}, false);
+
+document.addEventListener("dragend", function(event) {
+  // reset the transparency
+  event.target.style.opacity = "";
+}, false);
+
+/* events fired on the drop targets */
+document.addEventListener("dragover", function(event) {
+  // prevent default to allow drop
+  event.preventDefault();
+}, false);
+
+document.addEventListener("dragenter", function(event) {
+  // highlight potential drop target when the draggable element enters it
+  if (event.target.className == "dropzone") {
+    event.target.style.background = "purple";
+  }
+
+}, false);
+
+document.addEventListener("dragleave", function(event) {
+  // reset background of potential drop target when the draggable element leaves it
+  if (event.target.className == "dropzone") {
+    event.target.style.background = "";
+  }
+
+}, false);
+
+document.addEventListener("drop", function(event) {
+  // prevent default action (open as link for some elements)
+  event.preventDefault();
+  // move dragged elem to the selected drop target
+  if (event.target.className == "dropzone") {
+    event.target.style.background = "";
+    dragged.parentNode.removeChild( dragged );
+    event.target.appendChild( dragged );
+  }
+}, false);
+
+
+// 4. Keydown element
+
+// EXAMPLE:
+// eventTarget.addEventListener("keydown", event => {
+//     if (event.isComposing || event.keyCode === 229) {
+//       return;
+//     }
+//     // do something
+//   });
+
+const manDown = document.querySelector("body");
+manDown.addEventListener("keydown", event => {
+        if (event.isComposing || event.keyCode === 229) {
+          return;
+        }
+        console.log("this is the number you want");
+    });
+
+
+
+
+//   Using your index.js file, create 10 unique event listeners. using your creativity to make the Fun Bus site more interactive. Here are some unique events you could try to use:
+//  mouseover
+//  keydown
+//  wheel
+//  drag / drop
+//  load
+//  focus
+//  resize
+//  scroll
+//  select
+//  dblclick
+// Using the 10 unique events, find ways to update the DOM in creative ways. For example you could change colors, animate objects, remove objects, etc.
+
+// 5. Load
+// Example:
+// window.addEventListener('load', (event) => {
+//     console.log('page is fully loaded');
+//   });
+
+const log = document.querySelector('.event-log-contents');
+const reload = document.querySelector('#reload');
+
+reload.addEventListener('click', () => {
+  log.textContent ='';
+  window.setTimeout(() => {
+      window.location.reload(true);
+  }, 200);
+});
+
+window.addEventListener('load', (event) => {
+    log.textContent = log.textContent + 'load\n';
+});
+
+document.addEventListener('readystatechange', (event) => {
+    log.textContent = log.textContent + `we liveeeee?: ${document.readyState}\n`;
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    log.textContent = log.textContent + `DOMContentLoaded\n`;
+});
+
+window.addEventListener('load', (event) => {
+    console.log('page is fully loaded');
+  });
+
+
+
+// 6. Focus
